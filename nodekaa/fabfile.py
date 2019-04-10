6c230822cc16ab9f300dfd066b1f3541f48f2340
Ulas Baran Baloglu <39335306+ulasbaloglu@users.noreply.github.com>
2019-04-10 11:45:09 +0100

    Update fabfile.py

--- nodekaa/fabfile.py
+++ nodekaa/fabfile.py
@@ -27,37 +27,37 @@ def test(ctx):
 @task
 def servertasks(ctx):
 	with Connection(ctx.host, ctx.user, connect_kwargs=ctx.connect_kwargs) as conn:
-		conn.run('printf \"**************************** \n\"')
-		conn.run('printf \"*** Starting Preperation of the Server\n\"')
-		conn.run('printf \"**************************** \n\"')
+		sys.stdout.write("****************************\n")
+		sys.stdout.write("*** Starting Preperation of the Server\n")
+		sys.stdout.write("****************************\n")
 		conn.sudo('apt-get update')
 		conn.sudo('apt-get -y upgrade')
 		conn.sudo('mv /var/lib/dpkg/lock /var/lib/dpkg/lock_backup')
 		conn.sudo('apt-get install -y wget ca-certificates curl')
 		conn.sudo('rm -vf /var/lib/dpkg/lock_backup')
-		conn.run('printf \"*** Server prepared ***\n\"')
+		sys.stdout.write("*** Server prepared ***\n\n")
 		installjava(ctx)
 
 @task
 def installjava(ctx):
 	with Connection(ctx.host, ctx.user, connect_kwargs=ctx.connect_kwargs) as conn:
-		conn.run('printf \"**************************** \n\"')
-		conn.run('printf \"*** Installing Oracle JDK 8\n\"')
-		conn.run('printf \"**************************** \n\"')
+		sys.stdout.write("****************************\n")
+		sys.stdout.write("*** Installing Oracle JDK 8\n")
+		sys.stdout.write("****************************\n")
 		conn.sudo('add-apt-repository ppa:webupd8team/java')
 		conn.sudo('apt-get update')
 		conn.sudo('echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections')
 		conn.sudo('echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections')
 		conn.sudo('apt-get install -y oracle-java8-installer')
-		conn.run('printf \"*** Oracle JDK 8 installed ***\n\"')
+		sys.stdout.write("*** Oracle JDK 8 installed ***\n\n")
 		installmariadb(ctx)
 
 @task
 def installmariadb(ctx):
 	with Connection(ctx.host, ctx.user, connect_kwargs=ctx.connect_kwargs) as conn:
-		conn.run('printf \"**************************** \n\"')
-		conn.run('printf \"*** MariaDB 10.3\n\"')
-		conn.run('printf \"**************************** \n\"')
+		sys.stdout.write("****************************\n")
+		sys.stdout.write("*** MariaDB 10.3\n")
+		sys.stdout.write("****************************\n")
 		conn.sudo('apt-get -y install software-properties-common')
 		conn.sudo('apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8')
 		conn.sudo('add-apt-repository \'deb [arch=amd64,arm64,ppc64el] http://sfo1.mirrors.digitalocean.com/mariadb/repo/10.3/ubuntu bionic main\'')
@@ -69,43 +69,49 @@ def installmariadb(ctx):
 		conn.sudo('apt install -y libmariadb-dev')
 		conn.sudo('apt install -y libmariadb-dev-compat')
 		conn.sudo('apt-get install -y libmariadbclient18')
+		sys.stdout.write("****************************\n")
+		sys.stdout.write("*** Securing MYSQL installation\n")
+		sys.stdout.write("*** DO NOT change root password here, you can do it later in server console\n")
+		sys.stdout.write("*** Remove anonymous users, remove test database and reload privilage tables\n")
+		sys.stdout.write("****************************\n")
+		conn.sudo('sleep 6')
 		conn.sudo('mysql_secure_installation')
 		conn.sudo('netstat -ntlp | grep 3306')
-		conn.run('printf \"*** MariaDB 10.3 installed ***\n\"')		
+		sys.stdout.write("*** MariaDB 10.3 installed ***\n\n")		
 		createtables(ctx)
 
 @task
 def createtables(ctx):
 	with Connection(ctx.host, ctx.user, connect_kwargs=ctx.connect_kwargs) as conn:
-		conn.run('printf \"**************************** \n\"')
-		conn.run('printf \"*** Creation of SQL Tables for Kaa\n\"')
-		conn.run('printf \"**************************** \n\"')
+		sys.stdout.write("****************************\n")
+		sys.stdout.write("*** Creation of SQL Tables for Kaa\n")
+		sys.stdout.write("****************************\n")
 		conn.put(config._sections['node_kaa']['kaa_sqlfile'])
 		conn.run('chmod +x ' + config._sections['node_kaa']['kaa_sqlfile'])
 		conn.run('./' + config._sections['node_kaa']['kaa_sqlfile'] + ' ' + config._sections['node_kaa']['sql_password'])
 		conn.sudo('rm ' + config._sections['node_kaa']['kaa_sqlfile'])
-		conn.run('printf \"*** SQL tables for Kaa prepared ***\n\"')
+		sys.stdout.write("*** SQL tables for Kaa prepared ***\n\n")
 		installzookeeper(ctx)
 
 @task
 def installzookeeper(ctx):
 	with Connection(ctx.host, ctx.user, connect_kwargs=ctx.connect_kwargs) as conn:
-		conn.run('printf \"**************************** \n\"')
-		conn.run('printf \"*** Installing Zookeper\n\"')
-		conn.run('printf \"**************************** \n\"')
+		sys.stdout.write("****************************\n")
+		sys.stdout.write("*** Installing Zookeper\n")
+		sys.stdout.write("****************************\n")
 		conn.sudo('apt-get -y install zookeeper')
 		conn.sudo('/usr/share/zookeeper/bin/zkServer.sh start')
 		conn.sudo('sleep 3')
-		conn.run('printf \"*** Zookeper installed ***\n\"')
+		sys.stdout.write("*** Zookeper installed ***\n\n")
 		conn.run('netstat -ntlp | grep 2181')
 		installmongodb(ctx)
 
 @task
 def installmongodb(ctx):
 	with Connection(ctx.host, ctx.user, connect_kwargs=ctx.connect_kwargs) as conn:
-		conn.run('printf \"**************************** \n\"')
-		conn.run('printf \"*** Installing MongoDB\n\"')
-		conn.run('printf \"**************************** \n\"')
+		sys.stdout.write("****************************\n")
+		sys.stdout.write("*** Installing MongoDB\n")
+		sys.stdout.write("****************************\n")
 		conn.sudo('apt-get remove mongodb* --purge')
 		conn.sudo('apt-get update')
 		conn.sudo('apt-get install -y mongodb')
@@ -113,36 +119,36 @@ def installmongodb(ctx):
 		conn.sudo('systemctl status mongodb')
 		conn.sudo('sleep 1')
 		conn.run('mongo --eval \'db.runCommand({ connectionStatus: 1 })\'')
-		conn.run('printf \"*** MongoDB installed ***\n\"')
+		sys.stdout.write("*** MongoDB installed ***\n\n")
 		installkaaserver(ctx)
 
 @task
 def installkaaserver(ctx):
 	with Connection(ctx.host, ctx.user, connect_kwargs=ctx.connect_kwargs) as conn:
-		conn.run('printf \"**************************** \n\"')
-		conn.run('printf \"*** Installing Kaa Node\n\"')
-		conn.run('printf \"**************************** \n\"')
+		sys.stdout.write("****************************\n")
+		sys.stdout.write("*** Installing Kaa Node\n")
+		sys.stdout.write("****************************\n")
 		conn.put(config._sections['node_kaa']['kaa_tarfile'])
 		conn.sudo('tar -xvf kaa-deb-*.tar.gz')
 		conn.sudo('dpkg -i ./deb/kaa-node-*.deb')
 		conn.sudo('rm ./deb/flume*')
 		conn.sudo('rm ./deb/kaa*')
 		conn.sudo('rm ' + config._sections['node_kaa']['kaa_tarfile'])
-		conn.run('echo Controlling installation')
+		sys.stdout.write("Controlling installation\n")
 		conn.run('cat /etc/kaa-node/conf/admin-dao.properties | grep jdbc_username')
 		conn.run('cat /etc/kaa-node/conf/admin-dao.properties | grep jdbc_password')
 		conn.run('cat /etc/kaa-node/conf/sql-dao.properties | grep jdbc_username')
 		conn.run('cat /etc/kaa-node/conf/sql-dao.properties | grep jdbc_password')
 		conn.run('cat /etc/kaa-node/conf/nosql-dao.properties | grep nosql_db_provider_name')
-		conn.run('printf \"*** Kaa node installed ***\n\"')
+		sys.stdout.write("*** Kaa node installed ***\n\n")
 		configurekaaserver(ctx)
 
 @task
 def configurekaaserver(ctx):
 	with Connection(ctx.host, ctx.user, connect_kwargs=ctx.connect_kwargs) as conn:
-		conn.run('printf \"**************************** \n\"')
-		conn.run('printf \"*** Configuring Kaa Node\n\"')
-		conn.run('printf \"**************************** \n\"')
+		sys.stdout.write("****************************\n")
+		sys.stdout.write("*** Configuring Kaa Node\n")
+		sys.stdout.write("****************************\n")
 		conn.sudo('sed -i \'s/transport_public_interface=localhost/transport_public_interface='\
 		 + config._sections['node_kaa']['host'] + '/g\' /etc/kaa-node/conf/kaa-node.properties')
 		conn.sudo('iptables -I INPUT -p tcp -m tcp --dport 22 -j ACCEPT')
@@ -157,11 +163,11 @@ def configurekaaserver(ctx):
 		conn.sudo('ufw allow from any to any port 9997 proto tcp')
 		conn.sudo('iptables -I INPUT -p tcp -m tcp --dport 9999 -j ACCEPT')
 		conn.sudo('ufw allow from any to any port 9999 proto tcp')
-		conn.run('printf \"persistent installation\n\"')
+		sys.stdout.write("persistent installation\n")
 		conn.sudo('echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections')
 		conn.sudo('echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections')
 		conn.sudo('apt-get install -y iptables-persistent')
-		conn.run('printf \"starting persistent\n\"')
+		sys.stdout.write("starting persistent\n")
 		conn.sudo('service netfilter-persistent start')
 		conn.sudo('netfilter-persistent save')
 		restorekaaserver(ctx)
@@ -182,16 +188,20 @@ def restorekaaserver(ctx):
 		elif choice in no:
    			startkaaserver(ctx)
 		else:
-   			sys.stdout.write("Please respond with 'yes' or 'no'")
+   			sys.stdout.write("Please respond with 'yes' or 'no'\n")
    			restorekaaserver(ctx)
 
 @task
 def startkaaserver(ctx):
-	with Connection(ctx.host, ctx.user, connect_kwargs=ctx.connect_kwargs) as conn:		
-		conn.run('printf \"starting kaa node\n\"')
+	with Connection(ctx.host, ctx.user, connect_kwargs=ctx.connect_kwargs) as conn:
+		conn.sudo('rm /var/lib/apt/lists/lock')
+		conn.sudo('rm /var/cache/apt/archives/lock')
+		conn.sudo('rm /var/lib/dpkg/lock')
+		conn.sudo('dpkg --configure -a')
+		sys.stdout.write("starting kaa node\n")
 		conn.sudo('service kaa-node start')
-		conn.run('printf \"*** Kaa node configured ***\n\"')
-		conn.run('printf \"*** Node deployment finished ***\n\"')
-		conn.run('echo Open Administration UI http://' + config._sections['node_kaa']['host']\
-		 + ':8080/kaaAdmin')
+		sys.stdout.write("*** Kaa node configured ***\n")
+		sys.stdout.write("*** Node deployment finished ***\n")
+		sys.stdout.write("Open Administration UI http://" + config._sections['node_kaa']['host']\
+		 + ":8080/kaaAdmin\n")
 
