25415983a86d4689902f7f5556a3ad74b2b32d8a
Ulas Baran Baloglu <39335306+ulasbaloglu@users.noreply.github.com>
2019-04-16 13:46:37 +0100

    Update fabfile.py

--- nodekaa/fabfile.py
+++ nodekaa/fabfile.py
@@ -14,6 +14,17 @@ def deploy(ctx):
 	:return:
 	'''
 	staging(ctx)
+	servertasks(ctx)
+	installjava(ctx)
+	installmariadb(ctx)
+	securemariadb(ctx)
+	createkaatables(ctx)
+	installzookeeper(ctx)
+	installmongodb(ctx)
+	installkaanode(ctx)
+	configurekaanode(ctx)
+	initiatekaanode(ctx)
+	finishdeployment(ctx)
 
 @task
 def staging(ctx):
@@ -25,7 +36,11 @@ def staging(ctx):
 	ctx.user = config._sections['node_kaa']['user']
 	ctx.connect_kwargs = {"key_filename":[config._sections['node_kaa']['keyfile']]}
 	ctx.host = config._sections['node_kaa']['host'] + ':' + config._sections['node_kaa']['port']
-	servertasks(ctx)
+
+#@task
+#def test(ctx):
+#	with Connection(ctx.host, ctx.user, connect_kwargs=ctx.connect_kwargs) as conn:
+#		conn.sudo('ls -al')
 
 @task
 def servertasks(ctx):
@@ -43,7 +58,6 @@ def servertasks(ctx):
 		conn.sudo('apt-get install -y wget ca-certificates curl')
 		conn.sudo('rm -vf /var/lib/dpkg/lock_backup')
 		sys.stdout.write("*** Server prepared ***\n\n")
-		installjava(ctx)
 
 @task
 def installjava(ctx):
@@ -61,7 +75,6 @@ def installjava(ctx):
 		conn.sudo('echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections')
 		conn.sudo('apt-get install -y oracle-java8-installer')
 		sys.stdout.write("*** Oracle JDK 8 installed ***\n\n")
-		installmariadb(ctx)
 
 @task
 def installmariadb(ctx):
@@ -84,7 +97,6 @@ def installmariadb(ctx):
 		conn.sudo('apt install -y libmariadb-dev')
 		conn.sudo('apt install -y libmariadb-dev-compat')
 		conn.sudo('apt-get install -y libmariadbclient18')
-		securemariadb(ctx)
 
 @task
 def securemariadb(ctx):
@@ -103,10 +115,9 @@ def securemariadb(ctx):
 		 + 'n\ny\nn\ny\ny\n " | mysql_secure_installation')
 		conn.sudo('netstat -ntlp | grep 3306')
 		sys.stdout.write("*** MariaDB 10.3 installed ***\n\n")		
-		createtables(ctx)
 
 @task
-def createtables(ctx):
+def createkaatables(ctx):
 	'''
 	Create SQL Tables for Kaa
 	:return:
@@ -120,7 +131,6 @@ def createtables(ctx):
 		conn.run('./' + config._sections['node_kaa']['kaa_sqlfile'] + ' ' + config._sections['node_kaa']['sql_password'])
 		conn.sudo('rm ' + config._sections['node_kaa']['kaa_sqlfile'])
 		sys.stdout.write("*** SQL tables for Kaa prepared ***\n\n")
-		installzookeeper(ctx)
 
 @task
 def installzookeeper(ctx):
@@ -137,7 +147,6 @@ def installzookeeper(ctx):
 		conn.sudo('sleep 3')
 		sys.stdout.write("*** Zookeper installed ***\n\n")
 		conn.run('netstat -ntlp | grep 2181')
-		installmongodb(ctx)
 
 @task
 def installmongodb(ctx):
@@ -157,10 +166,9 @@ def installmongodb(ctx):
 		conn.sudo('sleep 1')
 		conn.run('mongo --eval \'db.runCommand({ connectionStatus: 1 })\'')
 		sys.stdout.write("*** MongoDB installed ***\n\n")
-		installkaaserver(ctx)
 
 @task
-def installkaaserver(ctx):
+def installkaanode(ctx):
 	'''
 	Install Kaa Node
 	:return:
@@ -182,10 +190,9 @@ def installkaaserver(ctx):
 		conn.run('cat /etc/kaa-node/conf/sql-dao.properties | grep jdbc_password')
 		conn.run('cat /etc/kaa-node/conf/nosql-dao.properties | grep nosql_db_provider_name')
 		sys.stdout.write("*** Kaa node installed ***\n\n")
-		configurekaaserver(ctx)
 
 @task
-def configurekaaserver(ctx):
+def configurekaanode(ctx):
 	'''
 	Configure Kaa Node
 	:return:
@@ -215,12 +222,11 @@ def configurekaaserver(ctx):
 		sys.stdout.write("starting persistent\n")
 		conn.sudo('service netfilter-persistent start')
 		conn.sudo('netfilter-persistent save')
-		restorekaaserver(ctx)
 
 @task
-def restorekaaserver(ctx):
+def initiatekaanode(ctx):
 	'''
-	Restore Kaa server from dump file
+	Initiate Kaa server from dump file or from stratch
 	:return:
 	'''	
 	with Connection(ctx.host, ctx.user, connect_kwargs=ctx.connect_kwargs) as conn:
@@ -233,28 +239,36 @@ def restorekaaserver(ctx):
 			conn.sudo('mysql -uroot -p' + config._sections['node_kaa']['sql_password']\
 			 + ' kaa < ' + config._sections['node_kaa']['kaa_dumpfile'])
 			conn.sudo('rm ' + config._sections['node_kaa']['kaa_dumpfile'])
-			startkaaserver(ctx)
+			startkaanode(ctx)
 		elif choice in no:
-   			startkaaserver(ctx)
+   			startkaanode(ctx)
 		else:
    			sys.stdout.write("Please respond with 'yes' or 'no'\n")
-   			restorekaaserver(ctx)
+   			initiatekaanode(ctx)
 
 @task
-def startkaaserver(ctx):
+def startkaanode(ctx):
 	'''
 	Start Kaa server
 	:return:
 	'''	
 	with Connection(ctx.host, ctx.user, connect_kwargs=ctx.connect_kwargs) as conn:
-		conn.sudo('rm /var/lib/apt/lists/lock')
-		conn.sudo('rm /var/cache/apt/archives/lock')
-		conn.sudo('rm /var/lib/dpkg/lock')
-		conn.sudo('dpkg --configure -a')
 		sys.stdout.write("starting kaa node\n")
 		conn.sudo('service kaa-node start')
 		sys.stdout.write("*** Kaa node configured ***\n")
 		sys.stdout.write("*** Node deployment finished ***\n")
 		sys.stdout.write("Open Administration UI http://" + config._sections['node_kaa']['host']\
 		 + ":8080/kaaAdmin\n")
+
+@task
+def finishdeployment(ctx):
+	'''
+	Final tasks for the deployment
+	:return:
+	'''
+	with Connection(ctx.host, ctx.user, connect_kwargs=ctx.connect_kwargs) as conn:
+		conn.sudo('rm /var/lib/apt/lists/lock')
+		conn.sudo('rm /var/cache/apt/archives/lock')
+		conn.sudo('rm /var/lib/dpkg/lock')
+		conn.sudo('dpkg --configure -a')
 
