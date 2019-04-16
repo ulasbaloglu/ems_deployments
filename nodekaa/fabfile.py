b2348ab99b801a355aa80cfd6016eb64b1ae05a1
Ulas Baran Baloglu <39335306+ulasbaloglu@users.noreply.github.com>
2019-04-16 13:47:58 +0100

    Update fabfile.py

--- nodekaa/fabfile.py
+++ nodekaa/fabfile.py
@@ -37,11 +37,6 @@ def staging(ctx):
 	ctx.connect_kwargs = {"key_filename":[config._sections['node_kaa']['keyfile']]}
 	ctx.host = config._sections['node_kaa']['host'] + ':' + config._sections['node_kaa']['port']
 
-#@task
-#def test(ctx):
-#	with Connection(ctx.host, ctx.user, connect_kwargs=ctx.connect_kwargs) as conn:
-#		conn.sudo('ls -al')
-
 @task
 def servertasks(ctx):
 	'''
